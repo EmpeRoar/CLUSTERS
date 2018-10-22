92d5f58927aedd3dbddd7b2515ea4c7aa45a8749
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-23 02:51:13 +0800

    cluster function init

--- CLUSTERS/Program.cs
+++ CLUSTERS/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Linq;
 
 namespace CLUSTERS
 {
     class Program
     {
+        static int[] Process(int[] numbers)
+        {
+            var batchSize = 3;
+            return numbers
+                    .Select((Value, Index) => new { Value, Index })
+                    .GroupBy(list => list.Index / batchSize)
+                    .Select(group => (group.Select(p => p.Value)).Sum())
+                    .OrderByDescending(sum => sum)
+                    .Take(3)
+                    .ToArray();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input1 = new int[] { 1, 2, 2, 5, 7, 3, 7, 8, 9, 26, 16, 1, 2, 3, 1, 4, 5, 1 };
+            var input2 = new int[] { 3, 7, 9, 5, 3, 56, 8, 0, 2, 56, 7, 6, 4 };
+
+            var result = Process(input2);
+
+            Console.WriteLine(string.Join(",", result));
+            Console.ReadLine();
         }
     }
 }
