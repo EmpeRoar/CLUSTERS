6095aa559d752e3197ad930776a3e2df1f51a249
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-23 02:49:53 +0800

    Add project files.

---
+++ CLUSTERS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CLUSTERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
